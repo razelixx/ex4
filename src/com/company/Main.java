6e69230e88b3e71d4e99ea477a16ab23919ff416
razelixx <67235257+razelixx@users.noreply.github.com>
2020-10-17 01:37:47 +0300

    Задача №4: Отработка поразрядных операций

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите число в двоичной системе счисления:");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str = in.readLine();
+
+        int n = 0;
+        for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+            n += (str.charAt(i) - '0') * j;
+        }
+        System.out.println("Введенное число в десятичной системе счисления: " + n);
+    }
+}
